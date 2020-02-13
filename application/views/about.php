885e52c191ea8b51977a65ee390f81673ecf4240
DelFolks1 <raja.simarmata23@gmail.com>
2020-02-13 14:48:27 +0700

    init awal

---
+++ application/views/about.php
@@ -0,0 +1,5 @@
+<head>
+<title>About Us</title>
+</head>
+<h1>Tentang</h1>
+<p>Website ini dibuat tanggal 13 Februari 2020</p>
---
+++ application/views/contact.php
@@ -0,0 +1,8 @@
+<head>
+<title>Contact Us</title>
+</head>
+<h1>Contact</h1>
+<p>Nama : Raja Permata Boy</p>
+<p>No HP : 085645575117</p>
+<p>Email : raja.simarmata23@gmail.com</p>
+<p>Blog : http://informaticsdude.blogspot.com/</p>
